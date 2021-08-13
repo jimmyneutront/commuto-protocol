a97ba73cfcbe3c551e72f1e33e913b9b599c4c4f
jimmyneutront <80171492+jimmyneutront@users.noreply.github.com>
2021-08-13 00:51:16 -0400

    Create dummy stablecoin contracts
    Create several basic ERC20 contracts to represent supported stablecoins

---
+++ USDT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor () public ERC20("Dai", "DAI") {
+    }
+}
