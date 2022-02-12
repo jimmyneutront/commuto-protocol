b75148d5353bcc414e1009dcc3f0e60e1f7cd494
jimmyneutront <w8250693648@protonmail.ch>
2022-02-11 20:08:24 -0500

    Replace cancelOffer tests with actual Python unit tests

---
+++ tests/CommutoCancelOfferTest.py
@@ -0,0 +1,135 @@
+import CommutoSwapTest
+from hexbytes import HexBytes
+from uuid import uuid4
+
+class CommutoCancelOfferTest(CommutoSwapTest.CommutoSwapTest):
+
+    def test_cancelOffer_existence_check(self):
+        #Ensure cancelOffer checks for offer existence
+        try:
+            tx_details = {
+                "from": self.taker_address
+            }
+            self.commuto_swap_contract.functions.cancelOffer(
+                HexBytes(uuid4().bytes)
+            ).transact(tx_details)
+            raise (Exception("test_cancelOffer_existence_check failed without raising exception"))
+        except ValueError as e:
+            # "e15":"An offer with the specified id does not exist"
+            if not "e15" in str(e):
+                raise e
+
+    def test_cancelOffer_caller_is_maker_check(self):
+        #Ensure cancelOffer checks that caller is maker
+        try:
+            newOfferID = HexBytes(uuid4().bytes)
+            tx_details = {
+                "from": self.maker_address,
+            }
+            newOffer = {
+                "isCreated": True,
+                "isTaken": True,
+                "maker": self.maker_address,
+                "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+                "protocolVersion": 1,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.openOffer(
+                newOfferID,
+                newOffer,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            self.commuto_swap_contract.functions.cancelOffer(
+                newOfferID,
+            ).transact(tx_details)
+            raise (Exception("test_cancelOffer_caller_is_maker_check failed without raising exception"))
+        except ValueError as e:
+            # "e17":"Offers can only be mutated by offer maker"
+            if not "e17" in str(e):
+                raise e
+
+    def test_cancelOffer_taken_offer_check(self):
+        #Ensure that taken offers cannot be canceled
+        try:
+            newOfferID = HexBytes(uuid4().bytes)
+            tx_details = {
+                "from": self.maker_address,
+            }
+            newOffer = {
+                "isCreated": True,
+                "isTaken": True,
+                "maker": self.maker_address,
+                "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+                "protocolVersion": 1,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.openOffer(
+                newOfferID,
+                newOffer,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            newSwap = {
+                "isCreated": False,
+                "requiresFill": True,
+                "maker": self.maker_address,
+                "makerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "taker": self.taker_address,
+                "takerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "takenSwapAmount": 100,
+                "serviceFeeAmount": 1,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethod": "USD-SWIFT".encode("utf-8"),
+                "protocolVersion": 1,
+                "isPaymentSent": True,
+                "isPaymentReceived": True,
+                "hasBuyerClosed": True,
+                "hasSellerClosed": True,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.takeOffer(
+                newOfferID,
+                newSwap,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.maker_address
+            }
+            self.commuto_swap_contract.functions.cancelOffer(
+                newOfferID
+            ).transact(tx_details)
+            raise (Exception("test_cancelOffer_taken_offer_check failed without raising exception"))
+        except ValueError as e:
+            # "e16":"Offer is taken and cannot be mutated"
+            if not "e16" in str(e):
+                raise e
