7307f903fd71df8ceffd2410414f6abe350cc9a8
jimmyneutront <w8250693648@protonmail.ch>
2022-03-10 12:23:32 -0500

    Ensure disputed swap can't be escalated given lack of counterparty response if counterparty has responded

--- tests/CommutoEscalateDisputeTest.py
+++ tests/CommutoEscalateDisputeTest.py
@@ -5,9 +5,7 @@ from uuid import uuid4
 
 # Note: this should probably be moved to a CommutoEscalateDisputeTest file
 
-# TODO: Ensure disputed swap can't be escalated given lack of maker response if maker has responded
 # TODO: Ensure disputed swap can only be escalated to token holders given lack of response from maker if > 1 week has passed
-# TODO: Ensure disputed swap can't be escalated given lack of taker response if taker has responded
 # TODO: Ensure disputed swap can only be escalated to token holders given lack of response from taker if > 1 week has passed
 # TODO: Ensure disputed swap can't be escalated for rejection if resolution proposal wasn't rejected
 # TODO: Ensure dispute escalation emits event
@@ -270,4 +268,196 @@ class CommutoEscalateDisputeTest(CommutoSwapTest.CommutoSwapTest):
         except ValueError as e:
             #"e71": "More blocks must be mined before swap can be escalated"
             if not "e71" in str(e):
+                raise e
+
+    def test_escalateDispute_no_maker_response_check(self):
+        #Ensure disputed swap can't be escalated given lack of maker response if maker has responded
+        try:
+            newOfferID = HexBytes(uuid4().bytes)
+            tx_details = {
+                "from": self.maker_address
+            }
+            newOffer = {
+                "isCreated": True,
+                "isTaken": True,
+                "maker": self.maker_address,
+                "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+                "protocolVersion": 1,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.openOffer(
+                newOfferID,
+                newOffer,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            newSwap = {
+                "isCreated": False,
+                "requiresFill": True,
+                "maker": self.maker_address,
+                "makerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "taker": self.taker_address,
+                "takerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "takenSwapAmount": 100,
+                "serviceFeeAmount": 1,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethod": "USD-SWIFT".encode("utf-8"),
+                "protocolVersion": 1,
+                "isPaymentSent": True,
+                "isPaymentReceived": True,
+                "hasBuyerClosed": True,
+                "hasSellerClosed": True,
+                "disputeRaiser": 0,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.takeOffer(
+                newOfferID,
+                newSwap,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.raiseDispute(
+                newOfferID,
+                self.dispute_agent_0,
+                self.dispute_agent_1,
+                self.dispute_agent_2
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.dispute_agent_0
+            }
+            self.commuto_swap_contract.functions.proposeResolution(newOfferID, 10, 10, 0).transact(tx_details)
+            tx_details = {
+                "from": self.dispute_agent_1
+            }
+            self.commuto_swap_contract.functions.proposeResolution(newOfferID, 10, 10, 0).transact(tx_details)
+            tx_details = {
+                "from": self.maker_address
+            }
+            self.commuto_swap_contract.functions.reactToResolutionProposal(newOfferID, 1).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            self.commuto_swap_contract.functions.reactToResolutionProposal(newOfferID, 1).transact(tx_details)
+            sleep(2)
+            tx_details = {
+                "from": self.taker_address
+            }
+            self.commuto_swap_contract.functions.escalateDispute(newOfferID, 2).transact(tx_details)
+            raise Exception("test_escalateDispute_no_maker_response_check failed without raising exception")
+        except ValueError as e:
+            #"e74": "Dispute cannot be escalated for lack of counterparty reaction if counterparty has reacted"
+            if not "e74" in str(e):
+                raise e
+
+    def test_escalateDispute_no_taker_response_check(self):
+        #Ensure disputed swap can't be escalated given lack of taker response if taker has responded
+        try:
+            newOfferID = HexBytes(uuid4().bytes)
+            tx_details = {
+                "from": self.maker_address
+            }
+            newOffer = {
+                "isCreated": True,
+                "isTaken": True,
+                "maker": self.maker_address,
+                "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+                "protocolVersion": 1,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.openOffer(
+                newOfferID,
+                newOffer,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            newSwap = {
+                "isCreated": False,
+                "requiresFill": True,
+                "maker": self.maker_address,
+                "makerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "taker": self.taker_address,
+                "takerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "takenSwapAmount": 100,
+                "serviceFeeAmount": 1,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethod": "USD-SWIFT".encode("utf-8"),
+                "protocolVersion": 1,
+                "isPaymentSent": True,
+                "isPaymentReceived": True,
+                "hasBuyerClosed": True,
+                "hasSellerClosed": True,
+                "disputeRaiser": 0,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.takeOffer(
+                newOfferID,
+                newSwap,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.raiseDispute(
+                newOfferID,
+                self.dispute_agent_0,
+                self.dispute_agent_1,
+                self.dispute_agent_2
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.dispute_agent_0
+            }
+            self.commuto_swap_contract.functions.proposeResolution(newOfferID, 10, 10, 0).transact(tx_details)
+            tx_details = {
+                "from": self.dispute_agent_1
+            }
+            self.commuto_swap_contract.functions.proposeResolution(newOfferID, 10, 10, 0).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            self.commuto_swap_contract.functions.reactToResolutionProposal(newOfferID, 1).transact(tx_details)
+            tx_details = {
+                "from": self.maker_address
+            }
+            self.commuto_swap_contract.functions.reactToResolutionProposal(newOfferID, 1).transact(tx_details)
+            sleep(2)
+            tx_details = {
+                "from": self.maker_address
+            }
+            self.commuto_swap_contract.functions.escalateDispute(newOfferID, 2).transact(tx_details)
+            raise Exception("test_escalateDispute_no_taker_response_check failed without raising exception")
+        except ValueError as e:
+            #"e74": "Dispute cannot be escalated for lack of counterparty reaction if counterparty has reacted"
+            if not "e74" in str(e):
                 raise e
