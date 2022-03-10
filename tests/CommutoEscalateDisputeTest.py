ad47eb1bd70d36ded691918617c77fb4a093f871
jimmyneutront <w8250693648@protonmail.ch>
2022-03-10 12:13:03 -0500

    Ensure disputed swap can only be escalated to token holders given nonmatching response from dispute agents if specified time period has passed

--- tests/CommutoEscalateDisputeTest.py
+++ tests/CommutoEscalateDisputeTest.py
@@ -5,9 +5,6 @@ from uuid import uuid4
 
 # Note: this should probably be moved to a CommutoEscalateDisputeTest file
 
-# TODO: Ensure disputed swap can't be escalated for lack of agreement if dispute agents are in agreement
-
-# TODO: Ensure disputed swap can only be escalated to token holders given nonmatching response from dispute agents if > 1 week has passed
 # TODO: Ensure disputed swap can't be escalated given lack of maker response if maker has responded
 # TODO: Ensure disputed swap can only be escalated to token holders given lack of response from maker if > 1 week has passed
 # TODO: Ensure disputed swap can't be escalated given lack of taker response if taker has responded
@@ -193,4 +190,84 @@ class CommutoEscalateDisputeTest(CommutoSwapTest.CommutoSwapTest):
         except ValueError as e:
             #"e73": "Dispute can't be escalated for lack of dispute agent response if dispute agents have agreed on resolution proposal"
             if not "e73" in str(e):
+                raise e
+
+    def test_escalateDispute_no_agreement_time_delay_check(self):
+        #Ensure disputed swap can only be escalated to token holders given nonmatching response from dispute agents if specified time period has passed
+        #NOTE: This test might sometimes fail if other tests cause enough blocks to be mined between dispute raising and the escalation attempt
+        try:
+            newOfferID = HexBytes(uuid4().bytes)
+            tx_details = {
+                "from": self.maker_address
+            }
+            newOffer = {
+                "isCreated": True,
+                "isTaken": True,
+                "maker": self.maker_address,
+                "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+                "protocolVersion": 1,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.openOffer(
+                newOfferID,
+                newOffer,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            newSwap = {
+                "isCreated": False,
+                "requiresFill": True,
+                "maker": self.maker_address,
+                "makerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "taker": self.taker_address,
+                "takerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 100,
+                "amountUpperBound": 100,
+                "securityDepositAmount": 10,
+                "takenSwapAmount": 100,
+                "serviceFeeAmount": 1,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethod": "USD-SWIFT".encode("utf-8"),
+                "protocolVersion": 1,
+                "isPaymentSent": True,
+                "isPaymentReceived": True,
+                "hasBuyerClosed": True,
+                "hasSellerClosed": True,
+                "disputeRaiser": 0,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                11,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.takeOffer(
+                newOfferID,
+                newSwap,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.raiseDispute(
+                newOfferID,
+                self.dispute_agent_0,
+                self.dispute_agent_1,
+                self.dispute_agent_2
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.maker_address
+            }
+            self.commuto_swap_contract.functions.escalateDispute(newOfferID, 1).transact(tx_details)
+            raise Exception("test_escalateDispute_no_agreement_time_delay_check failed without raising exception")
+        except ValueError as e:
+            #"e71": "More blocks must be mined before swap can be escalated"
+            if not "e71" in str(e):
                 raise e
