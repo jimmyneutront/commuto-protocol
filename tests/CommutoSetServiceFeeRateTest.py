97b323679824b89d37647b104627e517be492d11
jimmyneutront <w8250693648@protonmail.ch>
2022-03-30 23:52:20 -0400

    Test that setServiceFeeRate emits an event on success

--- tests/CommutoSetServiceFeeRateTest.py
+++ tests/CommutoSetServiceFeeRateTest.py
@@ -15,3 +15,15 @@ class CommutoSetServiceFeeRateTest(CommutoSwapTest.CommutoSwapTest):
             #"e79": "Only the current Timelock can call this function"
             if not "e79" in str(e):
                 raise e
+
+    def test_setServiceFeeRate_event_emission_check(self):
+        #Ensure setting the service fee rate emits an event
+        ServiceFeeRateChanged_filter = self.commuto_swap_contract.events.ServiceFeeRateChanged\
+            .createFilter(fromBlock="latest")
+        tx_details = {
+            "from": self.w3.eth.accounts[2],
+        }
+        self.commuto_swap_contract.functions.setServiceFeeRate(200).transact(tx_details)
+        events = ServiceFeeRateChanged_filter.get_new_entries()
+        self.assertEqual(len(events), 1)
+        self.assertEqual(events[0]["args"]["newServiceFeeRate"], 200)
