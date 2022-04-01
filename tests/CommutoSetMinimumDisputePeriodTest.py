07717e0a65e81a713355b60242a54ae71956ad18
jimmyneutront <w8250693648@protonmail.ch>
2022-03-31 20:11:41 -0400

    Ensure that setting minimum deposit period emits event

--- tests/CommutoSetMinimumDisputePeriodTest.py
+++ tests/CommutoSetMinimumDisputePeriodTest.py
@@ -16,3 +16,15 @@ class CommutoSetMinimumDisputePeriodTest(CommutoSwapTest.CommutoSwapTest):
             #"e79": "Only the current Timelock can call this function"
             if not "e79" in str(e):
                 raise e
+
+    def test_setMinimumDisputePeriod_event_emission_check(self):
+        #Ensure that setting the minimum deposit period emits an event
+        MinimumDisputePeriodChanged_filter = self.commuto_swap_contract.events.MinimumDisputePeriodChanged\
+            .createFilter(fromBlock="latest")
+        tx_details = {
+            "from": self.w3.eth.accounts[2],
+        }
+        self.commuto_swap_contract.functions.setMinimumDisputePeriod(10).transact(tx_details)
+        events = MinimumDisputePeriodChanged_filter.get_new_entries()
+        self.assertEqual(len(events), 1)
+        self.assertEqual(events[0]["args"]["newMinimumDisputePeriod"], 10)
