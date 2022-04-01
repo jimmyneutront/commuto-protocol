fece4e5e5a47ec87ea11beead8791c0eaa4a1a14
jimmyneutront <w8250693648@protonmail.ch>
2022-03-31 23:15:06 -0400

    Ensure that changing the timelock emits an event

--- tests/CommutoSwapChangeTimelockTest.py
+++ tests/CommutoSwapChangeTimelockTest.py
@@ -16,3 +16,16 @@ class CommutoSwapChangeTimelockTest(CommutoSwapTest.CommutoSwapTest):
             # "e79": "Only the current Timelock can call this function"
             if not "e79" in str(e):
                 raise e
+
+    def test_changeTimelock_event_emission_check(self):
+        #Ensure that changeTimelock emits TimelockChanged event upon successful change
+        TimelockChanged_event_filter = self.commuto_swap_contract.events.TimelockChanged\
+            .createFilter(fromBlock="latest")
+        tx_details = {
+            "from": self.w3.eth.accounts[2],
+        }
+        self.commuto_swap_contract.functions.changeTimelock(self.Timelock_contract.address).transact(tx_details)
+        events = TimelockChanged_event_filter.get_new_entries()
+        self.assertEqual(len(events), 1)
+        self.assertEqual(events[0]["args"]["oldTimelock"], self.w3.eth.accounts[2])
+        self.assertEqual(events[0]["args"]["newTimelock"], self.Timelock_contract.address)
