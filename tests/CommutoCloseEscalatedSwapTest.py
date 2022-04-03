650633bfdad36be64ab65736c68f9d240fa19805
jimmyneutront <w8250693648@protonmail.ch>
2022-04-02 22:20:13 -0400

    Ensure total payout amount must equal total without spent service fees

--- tests/CommutoCloseEscalatedSwapTest.py
+++ tests/CommutoCloseEscalatedSwapTest.py
@@ -103,3 +103,87 @@ class CommutoCloseEscalatedSwapTest(CommutoSwapTest.CommutoSwapTest):
         except ValueError as e:
             # "e70": "closeEscalatedSwap can only be called for escalated swaps"
             self.assertTrue("e70" in str(e))
+
+    def test_closeEscalatedSwap_total_payout_equals_total_unspent_locked(self):
+        # Ensure total payout amount in resolution proposals must equal total amount locked in escrow, minus spent service fees
+        try:
+            newOfferID = HexBytes(uuid4().bytes)
+            tx_details = {
+                "from": self.maker_address
+            }
+            newOffer = {
+                "isCreated": True,
+                "isTaken": True,
+                "maker": self.maker_address,
+                "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 10000,
+                "amountUpperBound": 10000,
+                "securityDepositAmount": 1000,
+                "serviceFeeRate": 100,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+                "protocolVersion": 1,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                1100,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.openOffer(
+                newOfferID,
+                newOffer,
+            ).transact(tx_details)
+            tx_details = {
+                "from": self.taker_address
+            }
+            newSwap = {
+                "isCreated": False,
+                "requiresFill": True,
+                "maker": self.maker_address,
+                "makerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "taker": self.taker_address,
+                "takerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+                "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+                "amountLowerBound": 10000,
+                "amountUpperBound": 10000,
+                "securityDepositAmount": 1000,
+                "takenSwapAmount": 10000,
+                "serviceFeeAmount": 100,
+                "serviceFeeRate": 100,
+                "direction": 1,
+                "price": HexBytes("a price here".encode("utf-8").hex()),
+                "settlementMethod": "USD-SWIFT".encode("utf-8"),
+                "protocolVersion": 1,
+                "isPaymentSent": True,
+                "isPaymentReceived": True,
+                "hasBuyerClosed": True,
+                "hasSellerClosed": True,
+                "disputeRaiser": 0,
+            }
+            self.test_dai_contract.functions.increaseAllowance(
+                self.commuto_swap_deployment_tx_receipt.contractAddress,
+                1100,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.takeOffer(
+                newOfferID,
+                newSwap,
+            ).transact(tx_details)
+            self.commuto_swap_contract.functions.raiseDispute(
+                newOfferID,
+                self.dispute_agent_0,
+                self.dispute_agent_1,
+                self.dispute_agent_2
+            ).transact(tx_details)
+            self.mine_blocks(5)
+            tx_details = {
+                "from": self.maker_address
+            }
+            self.commuto_swap_contract.functions.escalateDispute(newOfferID, 1).transact(tx_details)
+            tx_details = {
+                "from": self.w3.eth.accounts[2],
+            }
+            self.commuto_swap_contract.functions.closeEscalatedSwap(newOfferID, 500, 500, 500).transact(tx_details)
+        except ValueError as e:
+            # "e81": "Total payout amount must equal total without spent service fees"
+            self.assertTrue("e81" in str(e))
