52399ba0161eb578f22140588890f94968542723
jimmyneutront <w8250693648@protonmail.ch>
2022-06-07 15:30:49 -0400

    Implement IOSTestingServer, which sets up testing environments for iOS Commuto Interface tests.

---
+++ interface_tests/IOSCommutoSwapTest.py
@@ -0,0 +1,70 @@
+from uuid import uuid4
+from hexbytes import HexBytes
+from tests.CommutoSwapTest import CommutoSwapTest
+
+
+class IOSCommutoSwapTest(CommutoSwapTest):
+
+    def testBlockchainServiceListen(self):
+        maker_as_seller_swap_id = HexBytes(uuid4().bytes)
+        tx_details = {
+            "from": self.maker_address,
+        }
+        maker_as_seller_offer = {
+            "isCreated": True,
+            "isTaken": True,
+            "maker": self.maker_address,
+            "interfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+            "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+            "amountLowerBound": 10000,
+            "amountUpperBound": 10000,
+            "securityDepositAmount": 1000,
+            "serviceFeeRate": 100,
+            "direction": 1,
+            "price": HexBytes("a price here".encode("utf-8").hex()),
+            "settlementMethods": ["USD-SWIFT".encode("utf-8"), ],
+            "protocolVersion": 1,
+        }
+        self.test_dai_contract.functions.increaseAllowance(
+            self.commuto_swap_deployment_tx_receipt.contractAddress,
+            1100,
+        ).transact(tx_details)
+        self.commuto_swap_contract.functions.openOffer(
+            maker_as_seller_swap_id,
+            maker_as_seller_offer
+        ).transact(tx_details)
+        tx_details = {
+            "from": self.taker_address
+        }
+        maker_as_seller_swap = {
+            "isCreated": False,
+            "requiresFill": True,
+            "maker": self.maker_address,
+            "makerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+            "taker": self.taker_address,
+            "takerInterfaceId": HexBytes("an interface Id here".encode("utf-8").hex()),
+            "stablecoin": self.dai_deployment_tx_receipt.contractAddress,
+            "amountLowerBound": 10000,
+            "amountUpperBound": 10000,
+            "securityDepositAmount": 1000,
+            "takenSwapAmount": 10000,
+            "serviceFeeAmount": 100,
+            "serviceFeeRate": 100,
+            "direction": 1,
+            "price": HexBytes("a price here".encode("utf-8").hex()),
+            "settlementMethod": "USD-SWIFT".encode("utf-8"),
+            "protocolVersion": 1,
+            "isPaymentSent": True,
+            "isPaymentReceived": True,
+            "hasBuyerClosed": True,
+            "hasSellerClosed": True,
+            "disputeRaiser": 0,
+        }
+        self.test_dai_contract.functions.increaseAllowance(
+            self.commuto_swap_deployment_tx_receipt.contractAddress,
+            1100
+        ).transact(tx_details)
+        self.commuto_swap_contract.functions.takeOffer(
+            maker_as_seller_swap_id,
+            maker_as_seller_swap,
+        ).transact(tx_details)
---
+++ interface_tests/IOSTestingServer.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler
+from IOSCommutoSwapTest import IOSCommutoSwapTest
+import json
+
+
+class IOSTestingServer(BaseHTTPRequestHandler):
+    def set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    # noinspection PyPep8Naming
+    def do_GET(self):
+        if self.path.endswith('/test_blockchainservice_listen'):
+            self.set_headers()
+            commuto_swap_test = IOSCommutoSwapTest()
+            commuto_swap_test.setUp()
+            commuto_swap_test.testBlockchainServiceListen()
+            response = {
+                "commutoSwapAddress": str(commuto_swap_test.commuto_swap_contract.address),
+            }
+            self.wfile.write(bytes(json.dumps(response).encode()))
