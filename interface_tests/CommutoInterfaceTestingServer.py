21644f3e66ea201df9fac25e73e59498c5a9d051
jimmyneutront <w8250693648@protonmail.ch>
2022-09-10 23:21:40 -0400

    Add support for testing interface's SwapService.reportPaymentSent method

--- interface_tests/CommutoInterfaceTestingServer.py
+++ interface_tests/CommutoInterfaceTestingServer.py
@@ -263,6 +263,22 @@ class CommutoInterfaceTestingServer(BaseHTTPRequestHandler):
                     "stablecoinAddress": str(commuto_swap_test.test_dai_contract.address)
                 }
                 self.wfile.write(bytes(json.dumps(response).encode()))
+        elif self.path.__contains__('/test_swapservice_reportPaymentSent'):
+            query = urlparse(self.path).query
+            params = dict(parse_qsl(query))
+            self.set_headers()
+            swapID = UUID(params['swapID'])
+            if params['events'] == 'offer-opened-taken' and swapID is not None:
+                commuto_swap_test = InterfaceCommutoSwapTest()
+                commuto_swap_test.setUp()
+                commuto_swap_test.testSwapServiceReportPaymentSent(
+                    swap_id=swapID,
+                )
+                response = {
+                    "commutoSwapAddress": str(commuto_swap_test.commuto_swap_contract.address),
+                    "stablecoinAddress": str(commuto_swap_test.test_dai_contract.address)
+                }
+                self.wfile.write(bytes(json.dumps(response).encode()))
 
     # noinspection PyPep8Naming
     def do_POST(self):
